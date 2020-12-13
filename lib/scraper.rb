5aa51852eb5c36a23b665c1046671b73cd9df808
Blaire <grant.blaire@gmail.com>
2020-12-12 22:01:45 -0600

    commit tree c8ba55a158f88b4de22f97d9f17e8a8cb44ecf0b

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
+
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html) 
+
+#doc.css(".site-header__hero__headline").text.strip
+#doc.css(".text.title-oE5vT4").text.strip
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+  puts course.text.strip
+end 
